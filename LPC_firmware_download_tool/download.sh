24c847af34393afd774ff2da694cbc6343729a60
kenli <ken.li@nxp.com>
2023-06-08 12:17:41 +0800

    update downlaod.sh file

--- LPC_firmware_download_tool/download.sh
+++ LPC_firmware_download_tool/download.sh
@@ -1,5 +1,14 @@
 sudo dfu-util -d 1fc9:001f -D lpc54018_uart_server.bin
-sleep 5
+sleep 1
+echo "1s"
+sleep 1
+echo "2s"
+sleep 1
+echo "3s"
+sleep 1
+echo "4s"
+sleep 1
+echo "5s"
 ls /dev/ttyU*
 sleep 1
 sudo chmod -R 777 /dev/ttyU*
